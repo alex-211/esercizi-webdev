12950cfcdcbfd7aa31eede81f6e9a1fb8b2926bd
Alessandro Porpiglia <alessandropor@icloud.com>
2024-10-09 10:01:18 +0200

    es2 iniziato

---
+++ calcolatrice php e ajax/calcolatrice/script.js
@@ -0,0 +1,4 @@
+function riceviNumero () 
+{
+    alert("suca");
+}
--- esercizio ajax/htdocs/ajax/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if($_SERVER['REQUEST_METHOD']='POST')
-    {
-        $nome = $_POST['nome'];
-        $cognome = $_POST['cognome'];
-
-        echo "Ciao, ".htmlspecialchars($nome)." ".htmlspecialchars("$cognome");
-    }
-?>
