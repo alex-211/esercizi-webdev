26a14698a34702a8fa4a41ffdf1a4e5712efecb1
Alessandro Porpiglia <alessandro.porpiglia@itismajo.school>
2024-11-13 10:01:23 +0100

    mroe

--- login e server/Porpiglia5ainf/index.php
+++ login e server/Porpiglia5ainf/index.php
@@ -6,15 +6,16 @@
     <title>Boss Login - Porpiglia 5AINF</title>
 </head>
 <body>
-    <h1>php version</h1>
+    <h1>Login su Bossetti-Net</h1>
     <form action="server.php" method="post">
-        <label for="username">Username:</label>
+        <label for="username">Username o email:</label>
         <input type="text" name="username" id="username" required>
         <br>
         <label for="password">Password:</label>
         <input type="password" name="password" id="password" required>
         <br>
         <input type="submit" value="Login">
+        <button onclick="" value="Signup">
     </form>
 </body>
 </html>
--- login e server/Porpiglia5ainf/server.php
+++ login e server/Porpiglia5ainf/server.php
@@ -4,8 +4,9 @@ session_start();
 $servername = "localhost";
 $username = "adminBossetti";
 $password = "freeAstheWind";
+$database = "login";
 
-$connessione = new mysqli($servername, $username, $password);
+$connessione = new mysqli($servername, $username, $password, $database);
 if ($connessione->connect_error) 
 {
   die("non funge: " . $connessione->connect_error);
@@ -15,21 +16,19 @@ else
     //echo "funge"; 
 }
 
-// contrariamente a quanto dice la pagine, non funge e si ferma all'echo funge
-// probabile cambio l'index in php perché boh
-
 if ($_SERVER['REQUEST_METHOD'] == "POST")
 {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if ($username == "bossetti99" && $password == "innocenza")
+    $sql = "SELECT username, password, email FROM utenti WHERE (username = '$username' OR email = '$username') AND password = '$password'";
+
+    if ($check = $connessione->query($sql))
     {
-        $_SESSION['username'] = $username;
-        echo "real boss";
+        echo "bossetti net user";
     }
     else
     {
-        echo "not a real boss";
+        echo "not a bossetti net user";
     }
 }
